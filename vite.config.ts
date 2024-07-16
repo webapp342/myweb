81558f705cd70acebcc1af91f314a4de3c0c7862
19984646 <busetaktas45@gmail.com>
2024-07-17 03:02:49 +0330

    kullanici adi gosterme

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,9 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), basicSsl()],
+  
     build: {
       outDir: './docs'
     },
-  base: './'
-});
+    base: '/myweb/'
+  });
