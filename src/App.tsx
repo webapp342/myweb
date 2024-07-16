7313e532ea706296b3c4458c51714468246ec48c
19984646 <busetaktas45@gmail.com>
2024-07-17 02:50:01 +0330

    kullanici adi gosterme

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,13 @@ import TapToEarn from './TapToEarn';
 import { saveUserData } from './firestoreUtils';
 
 function App() {
-  const [username, setUsername] = useState('Kullanıcı adı yok');
-  const [userId, setUserId] = useState<number>(0); // Start with 0 instead of null
-  const [photoUrl, setPhotoUrl] = useState('default-profile-pic.png');
+  const [username, setUsername] = useState('');
+  const [userId, setUserId] = useState<number>(0);
+  const [photoUrl, setPhotoUrl] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
       const user = WebApp.initDataUnsafe?.user as ExtendedWebAppUser;
-
       if (user) {
         setUsername(user.username || 'Kullanıcı adı yok');
         setUserId(user.id);
@@ -24,15 +23,6 @@ function App() {
 
         // Save user data to Firestore
         await saveUserData(user);
-        sessionStorage.setItem('userData', JSON.stringify(user));
-      } else {
-        const savedUserData = sessionStorage.getItem('userData');
-        if (savedUserData) {
-          const parsedUser = JSON.parse(savedUserData);
-          setUsername(parsedUser.username || 'Kullanıcı adı yok');
-          setUserId(parsedUser.id); // Make sure parsedUser.id is a number
-          setPhotoUrl(parsedUser.photo ? parsedUser.photo.big_file_id : 'default-profile-pic.png');
-        }
       }
     };
 
@@ -40,22 +30,27 @@ function App() {
   }, []);
 
   return (
-    <Router>
-      <div>
-        <Link to="/user-data">
-          <button>Kullanıcı Verilerine Git</button>
-        </Link>
-        <Link to="/tap-to-earn">
-          <button>Oyna</button>
-        </Link>
+<Router basename="/myweb">
+<div>
+        <Link to="/profile">
+        <button>Profil Sayfasına Git</button>
+      </Link>
+      <Link to="/user-data">
+        <button>Kullanıcı Verilerine Git</button>
+      </Link>
+      <Link to="/tap-to-earn">
+      <button>oyna</button>
 
+      </Link>
+
+      
         <Routes>
-          <Route path="/" element={<Navigate to="/profile" />} />
-          <Route path="/profile" element={<UserProfilePage username={username} userId={userId} photoUrl={photoUrl} />} />
+        <Route path="/" element={<Navigate to="/profile" />} />
+        <Route path="/profile" element={<UserProfilePage username={username} userId={userId} photoUrl={photoUrl} />} />
           <Route path="/user-data" element={<UserData userId={userId.toString()} />} />
           <Route path="/tap-to-earn" element={<TapToEarn userId={userId.toString()} />} />
           <Route path="/user-details" element={<UserDetails />} />
-          <Route path="*" element={<Navigate to="/" />} />
+          <Route path="*" element={<Navigate to="/" />} /> {/* Redirect all unknown paths to Home */}
         </Routes>
       </div>
     </Router>
@@ -63,3 +58,5 @@ function App() {
 }
 
 export default App;
+
+
