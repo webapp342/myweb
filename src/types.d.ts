cc2796b7b70dc8a3385603d0f09371dd0e41c14a
19984646 <busetaktas45@gmail.com>
2024-07-17 00:41:19 +0330

    kullanici adi gosterme

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,10 +1,11 @@
-// types.d.ts
 import { WebAppUser } from '@twa-dev/sdk';
 
 export interface ExtendedWebAppUser extends WebAppUser {
   id: number; // Kullanıcı ID'sini number olarak tanımlayın
   username?: string;
-  
+  first_name: string; // Kullanıcının ilk adı
+  last_name?: string; // Kullanıcının soyadı
+  language_code?: string; // Kullanıcının dil kodu
   photo?: {
     small_file_id: string;
     big_file_id: string;
