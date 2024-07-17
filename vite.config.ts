c833ab0f42837a9ed68cde343f79625be43a41e6
19984646 <busetaktas45@gmail.com>
2024-07-17 03:32:37 +0330

    kullanici adi gosterme

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
-  
-    build: {
-      outDir: './docs'
-    },
-    base: '/myweb/'
-  });
+  plugins: [react()],
+  base: '/myweb/', // Make sure this matches your repo name
+});
